abf7716c02f79341ea9ab129df98390976630d86
Josi Whitlock <62673737+J051333@users.noreply.github.com>
2025-03-22 18:24:57 -0500

    Updated minsdk

--- tracker/app/build.gradle.kts
+++ tracker/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.hacksolotls.tracker"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
